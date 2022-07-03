b21b11bc1497d649305945c1cdf19cb29b5b1f11
Lalit-kumar001 <96826866+Lalit-kumar001@users.noreply.github.com>
2022-07-03 21:41:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -77,3 +77,14 @@ document.addEventListener('click',function(){
     document.querySelector('.i-7').style.display = "none";
     document.querySelector('.i-8').style.display = "none";
 })
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-200px";
+  }
+}
